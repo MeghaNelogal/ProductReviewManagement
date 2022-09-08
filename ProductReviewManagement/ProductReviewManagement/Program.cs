bb6bd80150bdc0c8f2916ef505a6ed8f01d4f0a5
Your Name <you@example.com>
2022-09-08 23:56:18 +0530

    [Megha] Add: Retrieve Record

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -19,25 +19,25 @@
             products.Add(new ProductModel() { ProductId = 9, UserId = 4, Rating = 5, IsLike = false, Review = "Bad" });
             products.Add(new ProductModel() { ProductId = 10, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 11, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
-            products.Add(new ProductModel() { ProductId = 12, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
+            products.Add(new ProductModel() { ProductId = 12, UserId = 2, Rating = 4, IsLike = false, Review = "nice" });
             products.Add(new ProductModel() { ProductId = 13, UserId = 7, Rating = 5, IsLike = true, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 14, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 15, UserId = 6, Rating = 3, IsLike = true, Review = "Bad" });
             products.Add(new ProductModel() { ProductId = 16, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 17, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 18, UserId = 7, Rating = 1, IsLike = false, Review = "Bad" });
-            products.Add(new ProductModel() { ProductId = 19, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
+            products.Add(new ProductModel() { ProductId = 1, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 20, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 21, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
-            products.Add(new ProductModel() { ProductId = 22, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
+            products.Add(new ProductModel() { ProductId = 1, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 23, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
-            products.Add(new ProductModel() { ProductId = 24, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
+            products.Add(new ProductModel() { ProductId = 2, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
             products.Add(new ProductModel() { ProductId = 25, UserId = 2, Rating = 4, IsLike = false, Review = "Good" });
 
             bool end = true;
             while (end)
             {
-                Console.WriteLine("\n1.Create Product Review\n2.Retrive Top Three Records \n3.Retrive Top Record With Product Id \n4.get top records\n5.get top five records \n6.create data table\n7.Like retrieve data\n8.Retrieve Average Rating\n9.Retrieve All Nice Reviews\n10.");
+                Console.WriteLine("\n1.Create Product Review\n2.Retrive Top Three Records \n3.Retrive Top Record With Product Id \n4.get top records\n5.get top five records \n6.create data table\n7.Like retrieve data\n8.Retrieve Average Rating\n9.Retrieve All Nice Reviews\n10.Retrieve User Id");
                 Console.WriteLine("\nEnter above Option");
                 int option = Convert.ToInt16(Console.ReadLine());
 
@@ -69,6 +69,9 @@
                         break;
                     case 9:
                         operation.RetrieveAllNiceReviews();
+                        break;
+                    case 10:
+                        operation.RetrieveAllProductReviews_ByUserIDAndOrderByRating();
                         break;
                     default:
                         Console.WriteLine("Enter above option");
