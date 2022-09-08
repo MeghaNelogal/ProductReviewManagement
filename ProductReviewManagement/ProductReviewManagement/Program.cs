265782b9d1419587aa2b63230c3e6f0196b5ed6a
Your Name <you@example.com>
2022-09-08 23:51:47 +0530

    [Megha] Add: Retrieve Nice Record

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -37,7 +37,7 @@
             bool end = true;
             while (end)
             {
-                Console.WriteLine("\n1.Create Product Review\n2.Retrive Top Three Records \n3.Retrive Top Record With Product Id \n4.get top records\n5.get top five records \n6.create data table\n7.Like retrieve data\n8.Retrieve Average Rating");
+                Console.WriteLine("\n1.Create Product Review\n2.Retrive Top Three Records \n3.Retrive Top Record With Product Id \n4.get top records\n5.get top five records \n6.create data table\n7.Like retrieve data\n8.Retrieve Average Rating\n9.Retrieve All Nice Reviews\n10.");
                 Console.WriteLine("\nEnter above Option");
                 int option = Convert.ToInt16(Console.ReadLine());
 
@@ -67,6 +67,9 @@
                     case 8:
                         operation.RetrieveAverageRating();
                         break;
+                    case 9:
+                        operation.RetrieveAllNiceReviews();
+                        break;
                     default:
                         Console.WriteLine("Enter above option");
                         break;
@@ -75,5 +78,3 @@
         }
     }
 }
-    
-
